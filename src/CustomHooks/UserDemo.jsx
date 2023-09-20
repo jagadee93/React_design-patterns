aa59c39307b55921f334561974b44d1a89d789e4
Jagadeesh Gongidi <jagadeeshgoud001@gmail.com>
2023-09-20 22:49:08 +0530

    added use resource hook

--- src/CustomHooks/UserDemo.jsx
+++ src/CustomHooks/UserDemo.jsx
@@ -1,7 +1,8 @@
 
-import { useUser } from './useUser';
+import { useResource } from './useResource';
+
 const UserDemo = ({ userId }) => {
-    const user = useUser(userId);
+    const user = useResource(`api/users/${userId}`);
     const { name, age } = user || {};
     console.log(user)
     return user ? (
