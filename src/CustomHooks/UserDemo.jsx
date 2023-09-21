a5ba41cc856917f9d932828071c74cba9b448603
Jagadeesh Gongidi <jagadeeshgoud001@gmail.com>
2023-09-21 12:13:27 +0530

    added datasource hook

--- src/CustomHooks/UserDemo.jsx
+++ src/CustomHooks/UserDemo.jsx
@@ -1,10 +1,25 @@
 
+import axios from 'axios';
+import { useDataSource } from './useDataSource';
 import { useResource } from './useResource';
 
+
+const serverResource = resourceUrl => async () => {
+    const response = await axios.get(resourceUrl);
+
+    return response.data;
+
+};
+
+
 const UserDemo = ({ userId }) => {
-    const user = useResource(`api/users/${userId}`);
+    // const user = useResource(`api/users/${userId}`);
+    const user = useDataSource(serverResource(`/api/users/${userId}`));
+
+
     const { name, age } = user || {};
     console.log(user)
+
     return user ? (
         <>
             <p>Name:{name},Age:{age} Years </p>
